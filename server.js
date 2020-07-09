3ba6e10351773df957a1c0ee964060ecc9567cee
cris2229 <cris.vasquez.cv56@gmail.com>
2020-07-09 12:55:57 +0800

    dfdsfa

--- server.js
+++ server.js
@@ -1,52 +1,16 @@
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 3000;
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
-    database: 'socapp'
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
 });
-
-connection.connect(err => {
-    if(err)
-    {
-        return err;
-    }
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-
-// this responds with "hello world" on the homepage
-app.get('/', function (req, res) {
-    connection.query('select * from post order by date_post DESC', (err, results) =>{
-        if (err) {
-            return res.send(err);
-        }else {
-            return res.json({
-                    data : results
-            })
-        }
-    });
-
-});
-
-app.post('/add',   (req, res) => {
-    const {email,name,message} = req.body.values;
-    var sql = "INSERT INTO post (email,name, post) VALUES ('"+email+"', '"+name+"','"+message+"')";
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-    res.send();
-});
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join('build', 'index.html'));
-    });
-}
+app.listen(port);
